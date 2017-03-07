f08cdcddc8431d76ac7e9f830b00d2e3d24dbae2
mikeadossi <mikeadossi@gmail.com>
2017-03-07 11:23:04 -0800

    curry_exercise tests passing

--- mag-part-1/curry/curry_exercises.js
+++ mag-part-1/curry/curry_exercises.js
@@ -6,24 +6,26 @@ var _ = require('ramda');
 //==============
 // Refactor to remove all arguments by partially applying the function
 
-var words = function(str) {
-  return split(' ', str);
-};
+var words = _.split(' ')
 
 // Exercise 1a
 //==============
 // Use map to make a new words fn that works on an array of strings.
 
-var sentences = undefined;
+var sentences = _.map(words)
+
+// sentences(["Jingle bells Batman smells", "Robin laid an egg"])
 
 
 // Exercise 2
 //==============
 // Refactor to remove all arguments by partially applying the functions
 
-var filterQs = function(xs) {
-  return filter(function(x){ return match(/q/i, x);  }, xs);
-};
+// var filterQs = function(xs) {
+//   return filter(function(x){ return match(/q/i, x);  }, xs);
+// };
+
+var filterQs = _.filter( match(/q/i) )
 
 
 // Exercise 3
@@ -33,27 +35,26 @@ var filterQs = function(xs) {
 // LEAVE BE:
 var _keepHighest = function(x,y){ return x >= y ? x : y; };
 
-// REFACTOR THIS ONE:
-var max = function(xs) {
-  return reduce(function(acc, x){
-    return _keepHighest(acc, x);
-  }, -Infinity, xs);
-};
 
-  
+var max = _.reduce(function(acc, x){
+  return _keepHighest(acc, x);
+}, -Infinity)
+
+
 // Bonus 1:
 // ============
 // wrap array's slice to be functional and curried.
 // //[1,2,3].slice(0, 2)
-var slice = undefined;
+var slice = _.slice()
+
 
 
 // Bonus 2:
 // ============
 // use slice to define a function "take" that takes n elements. Make it curried
-var take = undefined;
-
-
+var take = _.slice(0)
+//
+//
 module.exports = { words: words,
                    sentences: sentences,
                    filterQs: filterQs,
