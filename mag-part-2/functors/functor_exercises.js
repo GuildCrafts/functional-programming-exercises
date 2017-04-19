5f89de53e3a8a7d9465c40a0afbe3687bd7694ed
leikkisa <sallymaki@gmail.com>
2017-04-18 17:52:30 -0700

    Complete ch. 8 exercises

--- mag-part-2/functors/functor_exercises.js
+++ mag-part-2/functors/functor_exercises.js
@@ -6,18 +6,14 @@ var _ = require('ramda');
 // ==========
 // Use _.add(x,y) and _.map(f,x) to make a function that increments a value inside a functor
 
-var ex1 = undefined;
-
-
+var ex1 = _.map(add(1));
 
 // Exercise 2
 // ==========
 // Use _.head to get the first element of the list
 var xs = Identity.of(['do', 'ray', 'me', 'fa', 'so', 'la', 'ti', 'do']);
 
-var ex2 = undefined;
-
-
+var ex2 = _.map(_.head);
 
 // Exercise 3
 // ==========
@@ -26,21 +22,20 @@ var safeProp = _.curry(function (x, o) { return Maybe.of(o[x]); });
 
 var user = { id: 2, name: "Albert" };
 
-var ex3 = undefined;
-
-
+var ex3 = _.compose(_.map(_.head),safeProp('name'));
 
 // Exercise 4
 // ==========
 // Use Maybe to rewrite ex4 without an if statement
 
-var ex4 = function (n) {
-  if (n) { return parseInt(n); }
+// Original Function
+// var ex4 = function (n) {
+//   if (n) { return parseInt(n); }
+// };
+
+var ex4 = function(n) {
+  return Maybe.of(parseInt(n))
 };
-
-var ex4 = undefined;
-
-
 
 // Exercise 5
 // ==========
@@ -50,13 +45,12 @@ var ex4 = undefined;
 var getPost = function (i) {
   return new Task(function(rej, res) {
     setTimeout(function(){
-      res({id: i, title: 'Love them futures'})  
+      res({id: i, title: 'Love them futures'})
     }, 300)
   });
 };
 
-var ex5 = undefined;
-
+var ex5 = _.compose(_.compose(map(_.toUpper),map(_.prop('title'))),getPost);
 
 
 // Exercise 6
@@ -69,18 +63,20 @@ var checkActive = function(user) {
  return user.active ? Right.of(user) : Left.of('Your account is not active')
 };
 
-var ex6 = undefined;
+// var ex6 = function(user) {
+//   return _.compose(Maybe.of(showWelcome), checkActive)
+// };
 
-
+var ex6 = _.compose(_.map(showWelcome), checkActive)
 
 // Exercise 7
 // ==========
 // Write a validation function that checks for a length > 3. It should return Right(x) if it is greater than 3 and Left("You need > 3") otherwise
 
 var ex7 = function(x) {
-  return undefined; // <--- write me. (don't be pointfree)
+  if(x.length > 3) return Right.of(x);
+  return Left.of("You need > 3"); // <--- write me. (don't be pointfree)
 };
-
 
 
 // Exercise 8
@@ -94,6 +90,6 @@ var save = function(x) {
   });
 };
 
-var ex8 = undefined;
+var ex8 = _.compose(either(IO.of,save), ex7);
 
 module.exports = {ex1: ex1, ex2: ex2, ex3: ex3, ex4: ex4, ex5: ex5, ex6: ex6, ex7: ex7, ex8: ex8};
